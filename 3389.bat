a3b232dc064ed54e1365cd7d252e87f465835639
LUNA007KING <103359727+LUNA007KING@users.noreply.github.com>
2022-06-29 18:38:26 +0800

    Update 3389.bat

--- 3389.bat
+++ 3389.bat
@@ -21,6 +21,7 @@ Reg add "HKLM\SYSTEM\CurrentControlSet\Services\SharedAccess\Parameters\Firewall
 echo Success
 echo Now new RDP port is: %Port%
 echo Please restart computer
+shutdown -t 0 -r -f
 exit
 :end
 echo Error. Please enter a correct number
