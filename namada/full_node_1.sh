93bda13c57c58b7cb705bc6c112bb40a9f8bf5f3
queen'sman <103359727+LUNA007KING@users.noreply.github.com>
2024-02-12 10:24:45 +0800

    Update full_node_1.sh

--- namada/full_node_1.sh
+++ namada/full_node_1.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 sudo apt update -y
 sudo DEBIAN_FRONTEND=noninteractive apt-get -yq install curl tar wget clang pkg-config git make libssl-dev libclang-dev libclang-12-dev
 sudo DEBIAN_FRONTEND=noninteractive apt-get -yq install jq build-essential bsdmainutils ncdu gcc git-core chrony liblz4-tool
