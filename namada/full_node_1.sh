703076767d3693eaeb90ecd16d260cfc815f47f3
queen'sman <103359727+LUNA007KING@users.noreply.github.com>
2024-02-12 10:52:42 +0800

    Update full_node_1.sh

--- namada/full_node_1.sh
+++ namada/full_node_1.sh
@@ -3,4 +3,3 @@ sudo apt update -y
 sudo DEBIAN_FRONTEND=noninteractive apt-get -yq install curl tar wget clang pkg-config git make libssl-dev libclang-dev libclang-12-dev
 sudo DEBIAN_FRONTEND=noninteractive apt-get -yq install jq build-essential bsdmainutils ncdu gcc git-core chrony liblz4-tool
 sudo DEBIAN_FRONTEND=noninteractive apt-get -yq install original-awk uidmap dbus-user-session protobuf-compiler unzip libudev-dev
-reboot
