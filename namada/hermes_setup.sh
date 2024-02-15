892d1828386b24ca787327ec35f795741aad9dd3
queen'sman <103359727+LUNA007KING@users.noreply.github.com>
2024-02-15 11:41:30 +0800

    Create hermes_setup.sh

---
+++ namada/hermes_setup.sh
@@ -0,0 +1,35 @@
+echo "============================================================"
+echo "Install start"
+echo "============================================================"
+echo "Enter Namad Chain Id:"
+read NAMADA_CHAIN_ID
+echo "============================================================"
+echo "Enter Namada Trusting Period:"
+read NAMADA_TRUSTING_PERIOD
+echo "============================================================"
+echo "Enter Namada Relayer Key:"
+read RELAYER_KEY
+echo "============================================================"
+echo "Enter Namada Denom:"
+read NAMADA_DENOM
+echo "============================================================"
+
+echo export NAMADA_CHAIN_ID=${NAMADA_CHAIN_ID} >> $HOME/.bash_profile
+echo export NAMADA_TRUSTING_PERIOD=${NAMADA_TRUSTING_PERIOD} >> $HOME/.bash_profile
+echo export RELAYER_KEY=${RELAYER_KEY} >> $HOME/.bash_profile
+echo export NAMADA_DENOM=${NAMADA_DENOM} >> $HOME/.bash_profile
+source ~/.bash_profile
+
+wget https://github.com/heliaxdev/hermes/releases/download/v1.7.4-namada-beta7/hermes-v1.7.4-namada-beta7-x86_64-unknown-linux-gnu.zip
+sudo apt-get install unzip -y
+unzip hermes-v1.7.4-namada-beta7-x86_64-unknown-linux-gnu.zip
+cp hermes /usr/local/bin/
+rm -rf hermes-v1.7.4-namada-beta7-x86_64-unknown-linux-gnu.zip
+
+wget -O config.toml https://raw.githubusercontent.com/LUNA007KING/script/main/namada/hermes_config.toml
+sed -i 's/NAMADA_CHAIN_ID/$NAMADA_CHAIN_ID/g' config.toml
+sed -i 's/NAMADA_TRUSTING_PERIOD/$NAMADA_TRUSTING_PERIOD/g' config.toml
+sed -i 's/RELAYER_KEY/$RELAYER_KEY/g' config.toml
+sed -i 's/NAMADA_DENOM/$NAMADA_DENOM/g' config.toml
+
+cp config.toml /root/.hermes/config.toml
