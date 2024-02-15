392612f7fb6d6c0d6b4a90a937cc9e0c55d4c487
queen'sman <103359727+LUNA007KING@users.noreply.github.com>
2024-02-15 13:35:48 +0800

    Update hermes_setup.sh

--- namada/hermes_setup.sh
+++ namada/hermes_setup.sh
@@ -16,12 +16,20 @@ echo "============================================================"
 echo "Enter Osmo Relayer Seed:"
 read OSMO_RELAYER_SEED
 echo "============================================================"
+echo "Enter Osmo RPC Port:"
+read OSMO_RPC_PORT
+echo "============================================================"
+echo "Enter Osmo GRPC Port:"
+read OSMO_GRPC_PORT
+echo "============================================================"
 
 echo export NAMADA_CHAIN_ID=${NAMADA_CHAIN_ID} >> $HOME/.bash_profile
 echo export NAMADA_TRUSTING_PERIOD=${NAMADA_TRUSTING_PERIOD} >> $HOME/.bash_profile
 echo export NAMADA_RELAYER=${NAMADA_RELAYER} >> $HOME/.bash_profile
 echo export NAMADA_DENOM=${NAMADA_DENOM} >> $HOME/.bash_profile
 echo $OSMO_RELAYER_SEED > osmo_relayer_seed
+echo export OSMO_RPC_PORT=${OSMO_RPC_PORT} >> $HOME/.bash_profile
+echo export OSMO_GRPC_PORT=${OSMO_GRPC_PORT} >> $HOME/.bash_profile
 source ~/.bash_profile
 
 wget https://github.com/heliaxdev/hermes/releases/download/v1.7.4-namada-beta7/hermes-v1.7.4-namada-beta7-x86_64-unknown-linux-gnu.zip
@@ -34,6 +42,8 @@ sed -i "s/NAMADA_CHAIN_ID/$NAMADA_CHAIN_ID/g" config.toml
 sed -i "s/NAMADA_TRUSTING_PERIOD/$NAMADA_TRUSTING_PERIOD/g" config.toml
 sed -i "s/NAMADA_RELAYER/$NAMADA_RELAYER/g" config.toml
 sed -i "s/NAMADA_DENOM/$NAMADA_DENOM/g" config.toml
+sed -i "s/OSMO_RPC_PORT/$OSMO_RPC_PORT/g" config.toml
+sed -i "s/OSMO_GRPC_PORT/$OSMO_GRPC_PORT/g" config.toml
 
 mkdir /root/.hermes
 cp config.toml /root/.hermes/config.toml
