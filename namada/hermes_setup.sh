d3b298798edf773d4d58db3e59aaa9c4d0b20592
queen'sman <103359727+LUNA007KING@users.noreply.github.com>
2024-02-15 13:21:37 +0800

    Update hermes_setup.sh

--- namada/hermes_setup.sh
+++ namada/hermes_setup.sh
@@ -42,6 +42,4 @@ cp config.toml /root/.hermes/config.toml
 rm -rf hermes
 rm -rf config.toml
 hermes keys add --chain $NAMADA_CHAIN_ID --key-file $HOME/.local/share/namada/$NAMADA_CHAIN_ID/wallet.toml
-
-
 hermes keys add --chain osmo-test-5 --mnemonic-file osmo_relayer_seed
