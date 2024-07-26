436217fd47db7f8cb74648d78a9cc0b9d55ce985
doushiqq <103359727+yjl161@users.noreply.github.com>
2024-07-26 08:54:42 +0800

    Update clean.sh

--- allora/clean.sh
+++ allora/clean.sh
@@ -4,3 +4,5 @@ docker stop $(docker ps -aq) && docker rm $(docker ps -aq) && docker rmi -f $(do
 docker rm -f $(docker ps -a -q);docker system prune --volumes -a -f
 rm -rf basic-coin-prediction-node
 rm -rf allora-chain
+rm -rf worker_node_1.sh
+rm -rf worker_node_2.sh
