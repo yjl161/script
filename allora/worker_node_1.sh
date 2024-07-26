f46256798720a4bd427c22fc1b9597716974ab14
doushiqq <103359727+yjl161@users.noreply.github.com>
2024-07-26 09:19:17 +0800

    Update worker_node_1.sh

--- allora/worker_node_1.sh
+++ allora/worker_node_1.sh
@@ -21,7 +21,7 @@ sudo apt install -y ca-certificates zlib1g-dev libncurses5-dev libgdbm-dev libns
 sudo apt install -y python3 python3-pip
 
 # Install Docker
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor | sudo tee /usr/share/keyrings/docker-archive-keyring.gpg > /dev/null
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
 sudo apt-get install -y docker-ce docker-ce-cli containerd.io
