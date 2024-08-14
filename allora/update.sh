363832a394a8540add6e2a6798fe680ccf883aac
doushiqq <103359727+yjl161@users.noreply.github.com>
2024-08-14 09:19:27 +0800

    Create update.sh

---
+++ allora/update.sh
@@ -0,0 +1,56 @@
+cd $HOME
+git clone https://github.com/allora-network/basic-coin-prediction-node
+cd basic-coin-prediction-node
+
+rm -rf config.json
+nano config.json
+
+cat <<EOF > config.json
+{
+    "wallet": {
+        "addressKeyName": "$WALLET_NAME",
+        "addressRestoreMnemonic": "$ALLORA_SEED",
+        "alloraHomeDir": "",
+        "gas": "1000000",
+        "gasAdjustment": 1.0,
+        "nodeRpc": "https://sentries-rpc.testnet-1.testnet.allora.network/",
+        "maxRetries": 1,
+        "delay": 1,
+        "submitTx": false
+    },
+    "worker": [
+        {
+            "topicId": 1,
+            "inferenceEntrypointName": "api-worker-reputer",
+            "loopSeconds": 5,
+            "parameters": {
+                "InferenceEndpoint": "http://inference:8000/inference/{Token}",
+                "Token": "ETH"
+            }
+        },
+        {
+            "topicId": 2,
+            "inferenceEntrypointName": "api-worker-reputer",
+            "loopSeconds": 5,
+            "parameters": {
+                "InferenceEndpoint": "http://inference:8000/inference/{Token}",
+                "Token": "ETH"
+            }
+        },
+        {
+            "topicId": 7,
+            "inferenceEntrypointName": "api-worker-reputer",
+            "loopSeconds": 5,
+            "parameters": {
+                "InferenceEndpoint": "http://inference:8000/inference/{Token}",
+                "Token": "ETH"
+            }
+        }
+    ]
+}
+EOF
+
+chmod +x init.config
+./init.config
+
+docker compose up -d --build
